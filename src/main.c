7d223a2c491dbfd38d6873d0ae2a551707e1e11c
Qunerum <qunerumofficial@gmail.com>
2026-07-17 21:09:43 +0200

    update 2026-07-17 21:09

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include "fonts.h"
+
+int len(char* txt) { int i = 0; while(txt[i] != '\0') i+= txt[i]==' ' ? 3 : 1; return i/3; }
+
 int main() {
     Font font = doubleFrame;
-    printf("\n");
+    char c = 'A';
+    for (int i = 0; i < font.charHeight; i++) {
+        printf("%s", font.chars[c][i]);
+        int l = len(font.chars[c][i]);
+        for (int j = 0; j < font.charWidth-l; j++) printf(" ");
+        printf("| %i\n", l);
+    }
     return 0;
 }
